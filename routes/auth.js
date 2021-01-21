e734dcfa139e9452c9a780eabc15837584be4b81
mehdizademilad <milad.mehdizade1394@gmail.com>
2021-01-21 17:15:56 +0330

    Add User Register Route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const { userController } = require('../controller')
 
-router.post('/register', (req, res) => {
-    res.send("Good.");
+router.post('/register', async (req, res) => {
+    try {
+        let newUser = await userController.create(req.body);
+        res.send(newUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error');
+    }
 });
 
 router.post('/login', (req, res) => {
